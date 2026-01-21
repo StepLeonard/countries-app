28f3a8985124b5e131cd670807166d6702ee2fbf
stepleonard <stephanie.leonard716@gmail.com>
2026-01-21 14:13:35 -0800

    GET API for SavedCountries

--- version-2/src/pages/SavedCountries.jsx
+++ version-2/src/pages/SavedCountries.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // This page shows a form where the user types info
 function SavedCountries() {
+  // This holds the newest user from the API
+  const [newestUser, setNewestUser] = useState(null);
+
   // This runs when the user clicks the Submit button
   function handleSubmit(e) {
     e.preventDefault(); // stops the page from refreshing
@@ -25,11 +28,40 @@ function SavedCountries() {
     form.reset();
   }
 
+  // get newest user form data
+  async function getNewestUserData() {
+    try {
+      const response = await fetch(
+        "https://backend-answer-keys.onrender.com/get-newest-user",
+        {
+          method: "GET",
+        },
+      );
+
+      const data = await response.json();
+
+      // API sends an array, so we grab the first user
+      setNewestUser(data[0]);
+
+      console.log("Newest user from API:", data[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // Run the GET when the page loads
+  useEffect(() => {
+    getNewestUserData();
+  }, []);
+
   return (
     // This section holds everything on the page
     <section className="saved">
       {/* Page title */}
       <h1>Saved Countries</h1>
+
+      {/* Show newest user from API */}
+      {newestUser && <p>Welcome {newestUser.name}</p>}
 
       {/* This is the form */}
       <form className="form" onSubmit={handleSubmit}>
