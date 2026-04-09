fb599d2d7328b1a9213a19d58a7af1db99370ab9
stepleonard <stephanie.leonard716@gmail.com>
2026-04-09 12:06:32 -0700

    connected frontend to backend

--- version-4/client/vite.config.js
+++ version-4/client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000", // 👈 your backend
+        target: "https://countries-app-step.onrender.com", // 👈 your backend
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
